a0438e876b9fdc3eec9d6e06288f6b42c99467b6
sasha <sashapozhidaev07@example.com>
2022-05-20 14:50:36 +0600

    add soft deletes

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
@@ -11,4 +14,14 @@ class Comment extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded = [];
+
+    public function user(): Relation
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function post(): Relation
+    {
+        return $this->belongsTo(Post::class, 'post_id', 'id');
+    }
 }
